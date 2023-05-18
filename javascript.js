886b440b8a6dadeed70777622291dd5fe48bb168
Grelgn <grelgn@hotmail.com>
2023-05-18 18:54:24 +0300

    Play round

--- javascript.js
+++ javascript.js
@@ -27,4 +27,22 @@ function getPlayerChoice()
     return choice
 }
 // play round and declare winner
+function playRound()
+{
+    let computer = getComputerChoice()
+    let player = getPlayerChoice()
+    
+    if ((player === "rock" && computer === "scissors") || (player === "paper" && computer === "rock") || (player === "scissors" && computer === "paper")) 
+    {
+        return "player"
+    }
+    else if (player === computer) 
+    {
+        return "tie"
+    }
+    else
+    {
+        return "computer"
+    }
+}
 // play 5 times and keep score to report winner
