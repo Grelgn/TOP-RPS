62bcb2eb7955035c63709483a0cf2e52cb3bb963
Grelgn <grelgn@hotmail.com>
2023-05-26 20:41:32 +0300

    Button implementation

--- javascript.js
+++ javascript.js
@@ -4,40 +4,34 @@ function getComputerChoice()
     let randomize = Math.floor(Math.random() * 3)
     if (randomize === 0) 
     {
-        return "rock"    
+        return "Rock"    
     }
     else if (randomize === 1) 
     {
-        return "paper"    
+        return "Paper"    
     }
     else if (randomize === 2) 
     {
-        return "scissors"
+        return "Scissors"
     }
 }
+
 // get player choice
-function getPlayerChoice()
-{   
-    let choice = "NULL"
-    do {
-        let answer = prompt("Choose: Rock, Paper, Scissors")
-        choice = answer.toLowerCase()
-    } while (choice != "rock" && choice != "paper" && choice != "scissors");
-    
-    return choice
-}
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(btn => btn.addEventListener('click', playRound))
+
 // play round and declare winner
-function playRound()
+function playRound(e)
 {
     let computer = getComputerChoice()
-    let player = getPlayerChoice()
-    
-    if ((player === "rock" && computer === "scissors") || (player === "paper" && computer === "rock") || (player === "scissors" && computer === "paper")) 
+    let player = e.target.textContent
+
+    if ((player === "Rock" && computer === "Scissors") || (player === "Paper" && computer === "Rock") || (player === "Scissors" && computer === "Paper")) 
     {
         return "player"
     }
     else if (player === computer) 
-    {
+    {   
         return "tie"
     }
     else
@@ -45,46 +39,3 @@ function playRound()
         return "computer"
     }
 }
-// play 5 times and keep score to report winner
-function game()
-{
-    let player = 0, computer = 0, tie = 0
-    while (1) 
-    {   
-        let winner = playRound()
-        if (winner === "player") 
-        {
-            player++    
-        }
-        else if (winner === "computer") 
-        {
-            computer++   
-        }
-        else
-        {
-            tie++
-        }
-        if (player === 5 || computer === 5) 
-        {
-            break    
-        }
-    }
-
-    console.log("player: " + player)
-    console.log("computer: " + computer)
-    console.log("tie: " + tie)
-    if (player > computer) 
-    {
-        console.log("PLAYER WON")
-    }
-    else if (computer > player) 
-    {
-        console.log("COMPUTER WON")
-    }
-    else
-    {
-        console.log("TIE")
-    }
-}
-
-game()
